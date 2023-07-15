e74979e4234b2f7ab672648ebd522c339dda5e59
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-07-15 16:13:16 +0530

    controller

---
+++ src/main/java/com/project/proj/Controller/PostDetailsController.java
@@ -0,0 +1,33 @@
+package com.project.proj.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.proj.output.PostOfficeResponseBean;
+import com.project.proj.service.PostOfficeServiceImpl;
+import com.project.proj.service.iPostOffice;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/postal")
+public class PostDetailsController {
+
+    @Autowired
+    iPostOffice PostOfficeServiceImpl;
+    
+    @RequestMapping(value="/byCity", method=RequestMethod.GET,
+    consumes = MediaType.ALL_VALUE , produces = MediaType.APPLICATION_JSON_VALUE)
+    public PostOfficeResponseBean getPostByCity(@RequestParam String city) {
+        PostOfficeResponseBean postResponse;
+
+        postResponse=PostOfficeServiceImpl.fetchPostOfficeDetailsByCity(city);
+        return postResponse;
+    }
+    
+}
