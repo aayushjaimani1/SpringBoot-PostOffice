0bc666cbe7bfd2eccc351a17671d5439547bb3ec
Aayush Jaimani <66528266+aayushjaimani1@users.noreply.github.com>
2023-07-22 08:32:17 +0530

    changed service impl

--- src/main/java/com/project/proj/Service/PostOfficeServiceImpl.java
+++ src/main/java/com/project/proj/Service/PostOfficeServiceImpl.java
@@ -1,13 +1,12 @@
 package com.project.proj.Service;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.project.proj.output.PostOfficeDetailsBean;
+
 import com.project.proj.output.PostOfficeResponseBean;
 
 @Service("PostOfficeServiceImpl")
